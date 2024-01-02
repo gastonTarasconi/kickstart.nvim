0a2d980eb5a57d1aba8c204d8c700801536d3d85
Gastón Tarasconi <gaston.tarasconi@gmail.com>
2024-01-02 01:52:15 -0300

    config colorizer

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,3 +1,19 @@
+-- see the color of an hex #ffffff
 return {
-  'norcalli/nvim-colorizer.lua'
+  'norcalli/nvim-colorizer.lua',
+  event = "VeryLazy",
+  config = function()
+    -- See `:help telescope` and `:help telescope.setup()`
+    require('colorizer').setup {
+      'lua',
+      'css';
+      'javascript';
+      html = {
+        mode = 'foreground';
+      }
+    }
+
+    -- Enable telescope fzf native, if installed
+    pcall(require('telescope').load_extension, 'fzf')
+  end,
 }
